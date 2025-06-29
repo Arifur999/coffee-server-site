3dc569db9b30031757d3bc0089662a83855f3bbd
Arifur999 <arr9677@gmail.com>
2025-06-29 18:37:48 +0600

    complite

--- index.js
+++ index.js
@@ -69,14 +69,10 @@ res.send(result);
       const result = await coffeeCollection.deleteOne(query);
       res.send(result);
     });
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
